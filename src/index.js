eb6b0c7630bf2760c6720d829a09dd7e16ff0b07
chaimarajah <chaima.rajah@medtech.tn>
2023-07-07 02:12:56 +0200

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-console.log('Hello World !');
+console.log('Hello Wolrd !');
 
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const mongoose = require ('mongoose');
+const session = require('express-session')
 
-const App = express(); //starting our express app
 
 
-App.get('/' , (request, response) => {
-    return response.status(200).send('<h1>It works now ! </h1>');//200 for evy is OK
+const App = express();
+App.use(helmet());
+App.use(morgan('common'));
+App.use(express.json());
+App.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        secure: false,
+        httpOnly: true
+    }
+}));
 
-});
-App.listen(process.env.PORT, () => {
-    console.log(`Server running on http://localhost:${process.env.PORT}`);
+App.get('/', (request, response) => {
+    return response.status(200).send('<h1>It works From GET !</h1>'); //200 that says evy is ok
 });
 
 App.post('/', (request, response) => {
     return response.status(200).send('<h1>It works from POST !</h1>');
+});
+
+
+App.listen(process.env.PORT, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
