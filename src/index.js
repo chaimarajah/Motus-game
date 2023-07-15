2ee6cca51315d8b3cf7e18460e231acb6c72f959
chaimarajah <chaima.rajah@medtech.tn>
2023-07-15 17:26:36 +0200

    another new commit

--- src/index.js
+++ src/index.js
@@ -6,13 +6,10 @@ const morgan = require('morgan');
 const mongoose = require ('mongoose');
 const session = require('express-session');
 
-try {
-    mongoose.connect(process.env.MONGODB);
-    console.log("Connected to the db");
-} catch(error) {
-    console.error('Can\'t connect to the db');
+//mongoose.connect('mongodb://127.0.0.1:27017/myapp');
+mongoose.connect(process.env.MONGODB)
 
-}
+
 
 const App = express();
 App.use(helmet());
@@ -22,7 +19,7 @@ App.use(express.json());
 
 
 const GameRoutes = require('./routes/game');
-
+const WordRoutes = require('./routes/word');
 
 
 App.get('/', (request, response) => {
@@ -34,6 +31,7 @@ App.post('/', (request, response) => {
 });
 
 App.use('/game', GameRoutes);
+App.use('/word', WordRoutes);
 
 App.listen(process.env.PORT, () => {
     console.log(`Server running on http://localhost:${process.env.PORT}`);
--- src/routes/word.js
+++ src/routes/word.js
@@ -1,18 +1,15 @@
 const express = require('express');
  const Router = express.Router();
 
- const WordModel = require('../models/word');
 
+const WordModel = require('../models/word');
  Router.post('/', async (request, response) => {
      const { word } = request.body;
-
-     const wordModel = new WordModel({ 
-         name: word
-     });
-     await wordModel.save();
+     
 
      return response.status(200).json({
-         "msg": word
+        // "msg": "It works from word / post" 
+        "msg": word
      })
  });
 
